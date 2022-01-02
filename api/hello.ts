9db62726d9efa50459868078ffb7058378d6f414
tenja1918 <knjnrknr@gmail.com>
2022-01-02 22:35:11 +0900

    fix import. @vercel/node

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from "../node_modules/@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function (req: VercelRequest, res: VercelResponse) {
   const { name = "World" } = req.query;
